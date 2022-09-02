1357eaeeaebf490854bcb25117c7ae2b46874445
trc492 <trc492@live.com>
2022-09-01 17:51:55 -0700

    Created new TestOpMode for testing encoder reset.

--- TeamCode/src/main/java/team3543/TestOpMode.java
+++ TeamCode/src/main/java/team3543/TestOpMode.java
@@ -1,94 +1,126 @@
-package team3543;
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
+package teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-import TrcCommonLib.trclib.TrcDbgTrace;
-import TrcCommonLib.trclib.TrcGameController;
-import TrcCommonLib.trclib.TrcIntake;
-import TrcFtcLib.ftclib.FtcDashboard;
-import TrcFtcLib.ftclib.FtcGamepad;
-import TrcFtcLib.ftclib.FtcOpMode;
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
 
-@TeleOp(name="FtcTestOp", group="Ftc3543")
-public class TestOpMode extends FtcOpMode
-{
-    FtcDashboard dashboard;
-    FtcGamepad gamepad;
-    TrcIntake intake;
-    String intakeOwner = null;
+@TeleOp(name="Test OpMode", group="Test")
+//@Disabled
+public class TestOpmode extends LinearOpMode {
 
-    @Override
-    public void initRobot()
-    {
-        dashboard = FtcDashboard.getInstance();
-        gamepad = new FtcGamepad("GamePad", gamepad1, this::gamepadButtonEvent);
-        gamepad.setYInverted(true);
-        TrcIntake.Parameters intakeParams = new TrcIntake.Parameters()
-            .setMotorInverted(true)
-            .setTriggerInverted(true)
-            .setAnalogThreshold(3.9)
-            .setMsgTracer(TrcDbgTrace.getGlobalTracer());
-        intake = new Intake("intake", intakeParams).getTrcIntake();
-    }   //initRobot
+   // Declare OpMode members.
+   private ElapsedTime runtime = new ElapsedTime();
 
-    @Override
-    public void slowPeriodic(double elapsedTime)
-    {
-        if (intakeOwner == null)
-        {
-            intake.setPower(gamepad.getRightStickY(true));
-        }
-        dashboard.displayPrintf(1, "Distance: %.3f cm", intake.getSensorValue());
-    }   //slowPeriodic
+   @Override
+   public void runOpMode()
+   {
+      double lfEncTime = -1.0;
+      double rfEncTime = -1.0;
+      double lbEncTime = -1.0;
+      double rbEncTime = -1.0;
 
-    public void gamepadButtonEvent(TrcGameController gamepad, int button, boolean pressed)
-    {
-        final String ownerID = "autoAssistPickup";
+      DcMotor lfDrive  = hardwareMap.get(DcMotor.class, "lfWheel");
+      DcMotor rfDrive  = hardwareMap.get(DcMotor.class, "rfWheel");
+      DcMotor lbDrive  = hardwareMap.get(DcMotor.class, "lbWheel");
+      DcMotor rbDrive  = hardwareMap.get(DcMotor.class, "rbWheel");
 
-        dashboard.displayPrintf(
-            7, "%s: %04x->%s", gamepad, button, pressed? "Pressed": "Released");
-        switch (button)
-        {
-            case FtcGamepad.GAMEPAD_A:
-                if (pressed && intake.acquireExclusiveAccess(ownerID))
-                {
-                    intakeOwner = ownerID;
-                    intake.autoAssist(ownerID, RobotParams.INTAKE_POWER_PICKUP, null, this::intakeCompletion, 5.0);
-                }
-                break;
+      lfDrive.setDirection(DcMotor.Direction.REVERSE);
+      lbDrive.setDirection(DcMotor.Direction.REVERSE);
 
-            case FtcGamepad.GAMEPAD_B:
-                if (pressed && intake.acquireExclusiveAccess(ownerID))
-                {
-                    intakeOwner = ownerID;
-                    intake.autoAssist(ownerID, RobotParams.INTAKE_POWER_DUMP, null, this::intakeCompletion, 5.0);
-                }
-                break;
+      // Wait for the game to start (driver presses PLAY)
+      waitForStart();
+      runtime.reset();
 
-            case FtcGamepad.GAMEPAD_X:
-                break;
+      int lfEnc = lfDrive.getCurrentPosition();
+      int rfEnc = rfDrive.getCurrentPosition();
+      int lbEnc = lbDrive.getCurrentPosition();
+      int rbEnc = rbDrive.getCurrentPosition();
 
-            case FtcGamepad.GAMEPAD_Y:
-                break;
+      telemetry.addData("Start Encoders", "[%.3f]: lfEnc=%.0f, rfEnc=%.0f, lbEnc=%.0f, rbEnc=%.0f",
+                        runtime.seconds(), lfEnc, rfEnc, lbEnc, rbEnc);
+      telemetry.update();
 
-            case FtcGamepad.GAMEPAD_LBUMPER:
-                break;
+      lfDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      lbDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      rfDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+      rbDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-            case FtcGamepad.GAMEPAD_RBUMPER:
-                break;
+      // run until the end of the match (driver presses STOP)
+      while (opModeIsActive())
+      {
+         double currTime = runtime.seconds();
 
-            case FtcGamepad.GAMEPAD_DPAD_UP:
-                break;
+         if (lfEncTime < 0.0 && lfEnc == 0)
+         {
+            lfDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            lfEncTime = currTime;
+         }
 
-            case FtcGamepad.GAMEPAD_DPAD_DOWN:
-                break;
-        }
-    }   //gamepadButtonEvent
+         if (rfEncTime < 0.0 && rfEnc == 0)
+         {
+            rfDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rfEncTime = currTime;
+         }
 
-    private void intakeCompletion(Object context)
-    {
-        intake.releaseExclusiveAccess(intakeOwner);
-        intakeOwner = null;
-    }   //intakeCompletion
+         if (lbEncTime < 0.0 && lbEnc == 0)
+         {
+            lbDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            lbEncTime = currTime;
+         }
 
-}   //class TestOpMode
+         if (rbEncTime < 0.0 && rbEnc == 0)
+         {
+            rbDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rbEncTime = currTime;
+         }
+
+         telemetry.addData("Current Time", "time=%.3f", currTime);
+         telemetry.addData("Front Encoders", "lfEnc[%.3f]=%.0f, rfEnc[%.3f]=%.0f", lfEnc, lfEncTime, rfEnc, rfEncTime);
+         telemetry.addData(" Back Encoders", "lbEnc[%.3f]=%.0f, rbEnc[%.3f]=%.0f", lbEnc, lbEncTime, rbEnc, rbEncTime);
+         telemetry.update();
+      }
+   }
+}
